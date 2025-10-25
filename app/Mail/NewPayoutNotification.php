1888c94031574b948abe51ce051d270c1d3d4e03
RubberArchind <sean.makuto@gmail.com>
2025-10-25 13:56:56 +0700

    feat: should queue pay notification

--- app/Mail/NewPayoutNotification.php
+++ app/Mail/NewPayoutNotification.php
@@ -10,7 +10,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class NewPayoutNotification extends Mailable
+class NewPayoutNotification extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
